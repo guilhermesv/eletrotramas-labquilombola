8c1f9ea00fdd14b49943f78c6e3e57dd834b35f5
Guilherme Vieira <guilhermesv@gmail.com>
2025-07-13 17:51:29 -0300

    Update frequencia.ino

--- codigo-openlab/Instalacao/frequencia.ino
+++ codigo-openlab/Instalacao/frequencia.ino
@@ -1,12 +1,10 @@
-// TESTAR ADICIONAR O NOISE, MAS DEIXAR A FREQUENCIA SEM SE MOVIMENTAR, 
-// OU SE MOVIMENTAR MAIS DIVAGAR, 
 // TAMBEM SERIA LEGAL QUE A FREQ ADICIONASSE COR, 
 // ASSIM, ELA NAO APAGA PEDACOS
 
 void efeito_fft(CRGB *leds) {
 
   int frequencias_ignorar = 10;
-  int frequencias_divisor = 8; // Minimo 2, para ignorar valores negativos
+  int frequencias_divisor = 16; // Minimo 2, para ignorar valores negativos
   int amplitude_min = 2000;
   int amplitude_max = 10000;
 
@@ -34,7 +32,6 @@ void efeito_fft(CRGB *leds) {
       brightness = constrain(brightness, 0, 255);
       int sat = 255;
       int hue = mod8(t_1, 255);
-      hue = constrain(hue, 0, 255);
       leds[ledIndex] = CHSV(hue, sat, brightness);
     }
   }
