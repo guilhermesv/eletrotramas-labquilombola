78e99a616bd1e7b0b95d0b4f429b10c884efe1af
Guilherme Vieira <guilhermesv@gmail.com>
2025-08-02 10:16:16 -0300

    Correção na maneira de atualizar o brilho.
    Código anterior utilizava a função fadeToBlackBy(), que é referente a todos a array de leds, mas não atualizava o fade na array de brilho.

--- codigo-openlab/Teste-Adicionar-Brilho/Teste-Adicionar-Brilho.ino
+++ codigo-openlab/Teste-Adicionar-Brilho/Teste-Adicionar-Brilho.ino
@@ -1,16 +1,16 @@
 #include <FastLED.h>
 
 // DEFINICOES
-#define ledPin 4       // Porta Digital
-#define NUM_LEDS 1200  // Numero de leds
-CRGB leds[NUM_LEDS];   // Cria array de leds
-uint8_t brilho[NUM_LEDS] = {0};
+#define ledPin 4      // Porta Digital
+#define NUM_LEDS 192  // Numero de leds
+CRGB leds[NUM_LEDS];  // Cria array de leds
+uint8_t brilho[NUM_LEDS] = { 0 };
 int c = 0;
 
 uint8_t paleta_inicio = 0;
 DEFINE_GRADIENT_PALETTE(r_b){
-  0,   255,    0,    0,
-  255,   0,    0,   255,
+  0,  255,  0,  0,
+  255,  0,  0,  255,
 };
 CRGBPalette16 paleta = r_b;
 
@@ -20,40 +20,44 @@ void setup() {
   FastLED.show();
   Serial.begin(9600);
 
-  for(int i = 0; i < NUM_LEDS; i++) {
+  for (int i = 0; i < NUM_LEDS; i++) {
     brilho[i] = 0;
   }
 }
 
 void loop() {
 
-  int barra_tamanho = 15;
-  int passo = NUM_LEDS/barra_tamanho;
-  CRGB cor_inicial = ColorFromPalette(paleta, c%255, 255, LINEARBLEND);
+  // Atualiza a cor
+  CRGB cor_inicial = ColorFromPalette(paleta, c % 255, 255, LINEARBLEND);
   CHSV cor_hsv = rgb2hsv_approximate(cor_inicial);
 
+  // Atualizar o brilho N leds em sequência N vezes em posições aleatórias
+  int leds_aceso = 8;
+  int qtd_vezes = 4;
   EVERY_N_MILLISECONDS(1000) {
-  for(int i = 0; i < 10; i++) {
-    int posicao = random16(NUM_LEDS);
-    uint8_t posicao_brilho = random8();
-    for(int j = 0; j < barra_tamanho; j++) {
-      int led_indice = (posicao + j) % NUM_LEDS;
-      brilho[led_indice] = qadd8(brilho[led_indice], posicao_brilho);
-      leds[led_indice] = CHSV(cor_hsv.h, cor_hsv.s, brilho[led_indice]);
+    for (int i = 0; i < qtd_vezes; i++) {
+      int posicao = random16(NUM_LEDS);
+      uint8_t posicao_brilho = random8();
+      for (int j = 0; j < leds_aceso; j++) {
+        int led_indice = (posicao + j) % NUM_LEDS;
+        brilho[led_indice] = qadd8(brilho[led_indice], posicao_brilho);
+      }
     }
   }
-  }
-  
+
+  // Transfere as informações da array de Brilho para a fita e faz o fade
   EVERY_N_MILLISECONDS(20) {
-    fadeToBlackBy(leds, NUM_LEDS, 10);
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV(cor_hsv.h, cor_hsv.s, brilho[i]);
+      // Replica simples da função fadeToBlackBy() aplicada ao array de brilho
+      brilho[i] = scale8(brilho[i], 255 - 10);
+    }
   }
 
-  EVERY_N_MILLISECONDS(1000) {
+  EVERY_N_MILLISECONDS(500) {
     c++;
   }
 
-
   FastLED.show();
-
-  delay(50);
+  // delay(50);
 }
