55db61c56dd2e8a20b6965aa161e3fd45bda6444
Guilherme Vieira <guilhermesv@gmail.com>
2025-08-03 19:19:42 -0300

    Refatoração com várias correções

--- codigo-openlab/Instalacao-Padroes/main.ino
+++ codigo-openlab/Instalacao-Padroes/main.ino
@@ -16,77 +16,89 @@ void loop() {
   int intensidade = map(amplitude, 0, 3000, 0, 255);
   intensidade = constrain(intensidade, 0, 255);
 
-  // if (amplitude > 0 && amplitude <= FAIXA_A) {
-  //   for (int i = 0; i < 48; i++) {
-  //     int barra_offset = 0;
-  //     for (int i = 0; i < NUM_LEDS; i++) {
-  //       if(i % 48 == 0) {
-  //         barra_offset++;
-  //       }
-  //       int padrao_coluna = i % PADRAO_COL_QTD;
-  //       int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
-  //       if (padroes[1][padrao_linha][padrao_coluna] == regras[0][padrao_coluna] ) {
-  //         int led_indice = (i) % NUM_LEDS;
-  //         brilho[led_indice] = qadd8(brilho[led_indice], 255);
-  //         tom[led_indice] = 180;
-  //       }
-  //     }
-  //   }
-  // }
-
-  if (true) { //amplitude > FAIXA_A && amplitude <= FAIXA_B
-    for (int i = 48; i < 96; i++) {
-      int barra_offset = 0;
-      for (int i = 0; i < NUM_LEDS; i++) {
-        if(i % 48 == 0) {
-          barra_offset++;
-        }
-        int padrao_coluna = i % PADRAO_COL_QTD;
+  if (amplitude > 0 && amplitude <= FAIXA_A) {
+    int barra_offset = 0;
+    for (int j = 0; j < NUM_LEDS; j+=BAR_LEDS) {
+      for(int i = 0; i < BAR_LEDS; i++) {
+        int led_indice;
+        int padrao_coluna = (i) % PADRAO_COL_QTD;
+        int regra_coluna = (i) % REGRAS_TAM;
         int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
-        if (padroes[0][padrao_linha][padrao_coluna] == regras[0][padrao_coluna]) {
-          int led_indice;
-          // if(barra_offset % 2 == 0) {
-            led_indice = (i) % NUM_LEDS;
-          // } else {
-          //   led_indice = (48 - i) % NUM_LEDS;
-          // }
-          
-          brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
-          tom[led_indice] = 100;
-        } else {
-          brilho[i] = 0;
+        if (padroes[p][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
+          tom[led_indice] = 0;
+          if(barra_offset % 2 == 0) {
+            led_indice = j+i;
+            brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
+          } else {
+            led_indice = (j+BAR_LEDS-1)-i;
+            brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
+          }
         }
       }
+      barra_offset++;
+    }   
+  }
+
+  if (amplitude > FAIXA_A && amplitude <= FAIXA_B) {
+    int barra_offset = 0;
+    for (int j = 0; j < NUM_LEDS; j+=BAR_LEDS) {
+      for(int i = 0; i < BAR_LEDS; i++) {
+        int led_indice;
+        int padrao_coluna = (i) % PADRAO_COL_QTD;
+        int regra_coluna = (i) % REGRAS_TAM;
+        int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
+        if (padroes[(p+1)%PADROES_QTD][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
+          tom[led_indice] = 180;
+          if(barra_offset % 2 == 0) {
+            led_indice = j+i;
+            brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
+          } else {
+            led_indice = (j+BAR_LEDS-1)-i;
+            brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
+          }
+        }
+      }
+      barra_offset++;
     }
   }
 
-  // if (amplitude > FAIXA_B && amplitude <= FAIXA_C) {
-  //   for (int i = 96; i < 144; i++) {
-  //     int barra_offset = 0;
-  //     for (int i = 0; i < NUM_LEDS; i++) {
-  //       if(i % 48 == 0) {
-  //         barra_offset++;
-  //       }
-  //       int padrao_coluna = i % PADRAO_COL_QTD;
-  //       int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
-  //       if (padroes[p][padrao_linha][padrao_coluna] == regras[r][padrao_coluna] ) {
-  //         int led_indice = (i) % NUM_LEDS;
-  //         brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
-  //         tom[led_indice] = 0;
-  //       }
-  //     }
-  //   }
-  // }
+  if (amplitude > FAIXA_B && amplitude <= FAIXA_C) {
+    int barra_offset = 0;
+    for (int j = 0; j < NUM_LEDS; j+=BAR_LEDS) {
+      for(int i = 0; i < BAR_LEDS; i++) {
+        int led_indice;
+        int padrao_coluna = (i) % PADRAO_COL_QTD;
+        int regra_coluna = (i) % REGRAS_TAM;
+        int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
+        if (padroes[(p+2)%PADROES_QTD][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
+          tom[led_indice] = 100;
+          if(barra_offset % 2 == 0) {
+            led_indice = j+i;
+            brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
+          } else {
+            led_indice = (j+BAR_LEDS-1)-i;
+            brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
+          }
+        }
+      }
+      barra_offset++;
+    }
+  }
 
   
 
   EVERY_N_MILLISECONDS(20) {
     for (int i = 0; i < NUM_LEDS; i++) {
       leds[i] = CHSV(tom[i], 255, brilho[i]);
-      brilho[i] = scale8(brilho[i], 255 - 50);
+      brilho[i] = scale8(brilho[i], 255 - 80);
     }
-    // t++;
+    t++;
   }
+
+  EVERY_N_SECONDS(10) {
+    p = random8(PADROES_QTD);
+  }
+
 
   
   FastLED.show();
