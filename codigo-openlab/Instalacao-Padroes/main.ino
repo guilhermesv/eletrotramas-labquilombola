23347d25aa4bd10252b17581e88a95dcb1d9c164
Guilherme Vieira <guilhermesv@gmail.com>
2025-08-10 19:00:17 -0300

    Atualização de padrões, correção de bugs e novas lógicas de funcionamento

--- codigo-openlab/Instalacao-Padroes/main.ino
+++ codigo-openlab/Instalacao-Padroes/main.ino
@@ -5,10 +5,12 @@ void setup() {
   pinMode(MIC_PIN, INPUT);
   Serial.begin(115200);
 
-  // p = random8(PADROES_QTD);
+  p_A = random8(PADROES_QTD);
+  p_B = random8(PADROES_QTD);
+  p_C = random8(PADROES_QTD);
   // r = random8(REGRAS_QTD);
-
-  p = 6;
+  
+  // p = 6;
   r = 0;
 }
 
@@ -16,24 +18,27 @@ void loop() {
   // MIC
   // int amplitude = getAmplitude();  
   int amplitude = getSmoothedAmplitude();  
-  int intensidade = map(amplitude, 0, 3000, 0, 255);
-  intensidade = constrain(intensidade, 0, 255);
 
   if (amplitude > 0 && amplitude <= FAIXA_A) {
     int barra_offset = 0;
+    int intensidade = map(amplitude, 0, FAIXA_A, 0, 255);
+    intensidade = constrain(intensidade, 0, 255);
     for (int j = 0; j < NUM_LEDS; j+=BAR_LEDS) {
       for(int i = 0; i < BAR_LEDS; i++) {
         int led_indice;
         int padrao_coluna = (i) % PADRAO_COL_QTD;
         int regra_coluna = (i) % REGRAS_TAM;
         int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
-        if (padroes[p][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
-          tom[led_indice] = 0;
+        if (padroes[p_A][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
+          CRGB cor_atual = ColorFromPalette(pal_faixa_A, cor_faixa_A, 255, LINEARBLEND);
+          CHSV cor_atual_HSV = rgb2hsv_approximate(cor_atual);
           if(barra_offset % 2 == 0) {
             led_indice = j+i;
+            tom[led_indice] = cor_atual_HSV.hue;
             brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
           } else {
             led_indice = (j+BAR_LEDS-1)-i;
+            tom[led_indice] = cor_atual_HSV.hue;
             brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
           }
         }
@@ -44,19 +49,24 @@ void loop() {
 
   if (amplitude > FAIXA_A && amplitude <= FAIXA_B) {
     int barra_offset = 0;
+    int intensidade = map(amplitude, FAIXA_A, FAIXA_B, 0, 255);
+    intensidade = constrain(intensidade, 0, 255);
     for (int j = 0; j < NUM_LEDS; j+=BAR_LEDS) {
       for(int i = 0; i < BAR_LEDS; i++) {
         int led_indice;
         int padrao_coluna = (i) % PADRAO_COL_QTD;
         int regra_coluna = (i) % REGRAS_TAM;
         int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
-        if (padroes[(p+1)%PADROES_QTD][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
-          tom[led_indice] = 180;
+        if (padroes[(p_B)%PADROES_QTD][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
+          CRGB cor_atual = ColorFromPalette(pal_faixa_B, cor_faixa_B, 255, LINEARBLEND);
+          CHSV cor_atual_HSV = rgb2hsv_approximate(cor_atual);       
           if(barra_offset % 2 == 0) {
             led_indice = j+i;
+            tom[led_indice] = cor_atual_HSV.hue;
             brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
           } else {
             led_indice = (j+BAR_LEDS-1)-i;
+            tom[led_indice] = cor_atual_HSV.hue;
             brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
           }
         }
@@ -67,19 +77,24 @@ void loop() {
 
   if (amplitude > FAIXA_B && amplitude <= FAIXA_C) {
     int barra_offset = 0;
+    int intensidade = map(amplitude, FAIXA_B, FAIXA_C, 0, 255);
+    intensidade = constrain(intensidade, 0, 255);
     for (int j = 0; j < NUM_LEDS; j+=BAR_LEDS) {
       for(int i = 0; i < BAR_LEDS; i++) {
         int led_indice;
         int padrao_coluna = (i) % PADRAO_COL_QTD;
         int regra_coluna = (i) % REGRAS_TAM;
         int padrao_linha = (barra_offset + t) % PADRAO_LIN_QTD;
-        if (padroes[(p+2)%PADROES_QTD][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
-          tom[led_indice] = 100;
+        if (padroes[(p_C)%PADROES_QTD][padrao_linha][padrao_coluna] == regras[0][regra_coluna] ) {
+          CRGB cor_atual = ColorFromPalette(pal_faixa_C, cor_faixa_C, 255, LINEARBLEND);
+          CHSV cor_atual_HSV = rgb2hsv_approximate(cor_atual);
           if(barra_offset % 2 == 0) {
             led_indice = j+i;
+            tom[led_indice] = cor_atual_HSV.hue;
             brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
           } else {
             led_indice = (j+BAR_LEDS-1)-i;
+            tom[led_indice] = cor_atual_HSV.hue;
             brilho[led_indice] = qadd8(brilho[led_indice], intensidade);
           }
         }
@@ -88,22 +103,31 @@ void loop() {
     }
   }
 
-  
-
   EVERY_N_MILLISECONDS(20) {
     for (int i = 0; i < NUM_LEDS; i++) {
       leds[i] = CHSV(tom[i], 255, brilho[i]);
       brilho[i] = scale8(brilho[i], 255 - 80);
     }
+  }
+
+  EVERY_N_MILLISECONDS(100) {
     t++;
   }
 
-  EVERY_N_SECONDS(10) {
-    // p = random8(PADROES_QTD);
+  EVERY_N_SECONDS(8) {
+    p_A = random8(PADROES_QTD);
+    p_B = random8(PADROES_QTD);
+    p_C = random8(PADROES_QTD);
+
+    // p_A = (p_A+1) % PADROES_QTD;
+    // p_B = (p_A+1) % PADROES_QTD;
+    // p_C = (p_B+1) % PADROES_QTD;
+
+    cor_faixa_A = (cor_faixa_A + 1)%255;
+    cor_faixa_B = (cor_faixa_B + 1)%255;
+    cor_faixa_C = (cor_faixa_C + 1)%255;
   }
 
-
-  
   FastLED.show();
   delay(50);
 
